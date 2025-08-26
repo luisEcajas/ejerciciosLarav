f37e4475d0bfebc5634d11ee42d592889e71bb2c
luisEcajas <kikecajas18@gmail.com>
2025-08-26 13:35:02 -0500

    Update web.php

--- larav3066446/routes/web.php
+++ larav3066446/routes/web.php
@@ -10,18 +10,18 @@ Route::post('adivinar', [juegoController::class, 'adivinarNum'])->name('adivinar
 
 //Ejercicio2
 Route::get('frmFactorial',[juegoController::class,'frmFactorialNum']);
-Route::get('factorial',[juegoController::class,'factorialNumero'])->name('factorial.calcular');
+Route::post('factorial',[juegoController::class,'factorialNumero'])->name('factorial.calcular');
 //Ejercicio 3: 
 
-Route::get('frmNotas', [OperacionesController::class, 'frmNotas']);
-Route::post('notas', [OperacionesController::class, 'notas'])->name('notas.procesar');
+Route::get('frmNotas', [juegoController::class, 'frmNotas']);
+Route::post('notas', [juegoController::class, 'notas'])->name('notas.procesar');
 
 //Ejercicio 4: 
 
-Route::get('frmMultiplicar', [OperacionesController::class, 'frmMultiplicar']);
-Route::post('multiplicar', [OperacionesController::class, 'multiplicar'])->name('multiplicar.procesar');
+Route::get('frmMultiplicar', [juegoController::class, 'frmMultiplicar']);
+Route::post('multiplicar', [juegoController::class, 'multiplicar'])->name('multiplicar.procesar');
 
 //Ejercicio 5: 
 
-Route::get('frm20Nums', [OperacionesController::class, 'frm20Nums']);
-Route::post('20Nums', [OperacionesController::class, 'F20Nums'])->name('F20Nums.procesar');
+Route::get('frm20Nums', [juegoController::class, 'frm20Nums']);
+Route::post('20Nums', [juegoController::class, 'F20Nums'])->name('F20Nums.procesar');
